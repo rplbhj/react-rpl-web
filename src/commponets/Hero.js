653e2ad41af8ed6c0b361913fb96d77db29994c4
Ariffadillahh <fadilaharif99@gmail.com>
2022-06-01 20:50:43 +0700

    update hero

--- src/commponets/Hero.js
+++ src/commponets/Hero.js
@@ -1,15 +1,13 @@
 import React from "react";
-import Typed from "react-typed";
 
 const Hero = () => {
   return (
     <div>
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <p className="text-[#7AAEC5] text-lg uppercase font-bold">smk bhumi husada</p>
-        <h1 className="text-4xl font-bold uppercase pt-3">we are rpl</h1>
+        <p className="text-[#7AAEC5] text-lg uppercase font-bold md:text-2xl">smk bhumi husada</p>
+        <h1 className="text-2xl font-bold uppercase pt-3 md:text-4xl">we are software engineer</h1>
         <div>
           <h3 className="text-lg pt-3 capitalize text-slate-600">mempelajari seperti</h3>
-          <Typed strings={["hal0", "guys", "apa.", "kabs", "bro"]} typeSpeed={40} backSpeed={140} loop />
         </div>
       </div>
     </div>
